22200af0515d766187227feb66b42afc3a68b8ef
Jacob Glueck <swimgiraffe435@gmail.com>
2017-10-19 08:37:21 -0400

    preview

--- lab4/vga/img_to_rom.py
+++ lab4/vga/img_to_rom.py
@@ -14,15 +14,20 @@ def main():
 
     im = Image.open(args.img)
     im.thumbnail(size, Image.ANTIALIAS)
+    pixels = im.load()
+    im.show()
     for y in range(0, 128):
         for x in range(0, 128):
             color = (0, 0, 0)
             if x < im.size[0] and y < im.size[1]:
-                color = im.getpixel((x,y))[0:3]
+                color = pixels[x,y][0:3]
+                r, g, b = color
+                pixels[x,y] = ( (r >> 5) << 5, (g >> 5) << 5, (b >> 6) << 6 )
             r, g, b = color
             # print(color)
             hexc = (r >> 5) << 5 | (g >> 5) << 3 | (b >> 6)
             print(hex(hexc)[2:])
+    im.show()
 
 if __name__ == '__main__':
     main()
